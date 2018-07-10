b7f70e6b482400bc53abf76bdd6b03417214d83b
Sam Chung <tieoneease@gmail.com>
2018-07-10 14:21:47 -0700

    BOILERPLAET

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,17 @@
 'use strict';
 
-console.log('hello transpile');
+var _papaparse = require('papaparse');
 
-var c = 'shit';
-console.log(c);
+var _papaparse2 = _interopRequireDefault(_papaparse);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var productsFileInput = document.getElementById('products-file-input');
+var productsFileUploadButton = document.getElementById('products-file-upload-button');
+
+var tasksFileInput = document.getElementById('tasks-file-input');
+var tasksFileUploadButton = document.getElementById('tasks-file-upload-button');
+
+productsFileInput.onchange = function () {
+    var productsCsvFile = this.files[0];
+};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-console.log('hello transpile');
+import Papa from 'papaparse';
 
-let c = 'shit';
-console.log(c);
+const productsFileInput = document.getElementById('products-file-input');
+const productsFileUploadButton = document.getElementById('products-file-upload-button');
+
+const tasksFileInput = document.getElementById('tasks-file-input');
+const tasksFileUploadButton = document.getElementById('tasks-file-upload-button');
+
+productsFileInput.onchange = function() {
+    let productsCsvFile = this.files[0];
+
+}
